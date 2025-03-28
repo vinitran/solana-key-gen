cde2b1c819d0d720e6ca9c36955ee13b8349fb20
Vini Tran <vinhtran@vinitran.local>
2025-03-28 11:22:57 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,25 @@
+module main
+
+go 1.23.4
+
+require (
+	github.com/blocto/solana-go-sdk v1.30.0
+	github.com/mr-tron/base58 v1.2.0
+)
+
+require (
+	filippo.io/edwards25519 v1.0.0-rc.1 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgx/v5 v5.5.5 // indirect
+	github.com/jackc/puddle/v2 v2.2.1 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/stretchr/testify v1.8.1 // indirect
+	golang.org/x/crypto v0.17.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	gorm.io/driver/postgres v1.5.11 // indirect
+	gorm.io/gorm v1.25.12 // indirect
+)
---
+++ main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"fmt"
+	"github.com/google/uuid"
+	"log"
+	"sync"
+	"time"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+
+	"github.com/blocto/solana-go-sdk/types"
+	"github.com/mr-tron/base58/base58"
+)
+
+type TokenKey struct {
+	ID         string    `gorm:"type:uuid;default:gen_random_uuid();primaryKey"`
+	PrivateKey string    `gorm:"unique;column:private_key"`
+	PublicKey  string    `gorm:"unique;column:public_key"`
+	IsPicked   bool      `gorm:"column:is_picked;default:false"`
+	CreatedAt  time.Time `gorm:"column:created_at;default:now()"`
+}
+
+func (TokenKey) TableName() string {
+	return "token_key"
+}
+
+func connectDB() *gorm.DB {
+	dsn := "postgresql://postgres:123456@127.0.0.1:5432/postgres?sslmode=disable"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
+	return db
+}
+
+func generateVanityKeypair(suffix string) (string, string) {
+	var mu sync.Mutex
+	var account types.Account
+	found := make(chan struct{}, 1)
+	numWorkers := 4
+
+	for i := 0; i < numWorkers; i++ {
+		go func() {
+			for {
+				select {
+				case <-found:
+					return
+				default:
+					newAccount := types.NewAccount()
+					pubkeyString := base58.Encode(newAccount.PublicKey[:])
+					if len(pubkeyString) >= len(suffix) && pubkeyString[len(pubkeyString)-len(suffix):] == suffix {
+						mu.Lock()
+						account = newAccount
+						mu.Unlock()
+						select {
+						case found <- struct{}{}:
+						default:
+						}
+						return
+					}
+				}
+			}
+		}()
+	}
+
+	<-found
+	privateKey := base58.Encode(account.PrivateKey[:])
+	publicKey := base58.Encode(account.PublicKey[:])
+	return privateKey, publicKey
+}
+
+func checkAndGenerateKeys(db *gorm.DB) {
+	for {
+		var count int64
+		db.Model(&TokenKey{}).Count(&count)
+
+		fmt.Println("Current number of keys:", count)
+		if count < 50 {
+			privateKey, publicKey := generateVanityKeypair("ponz")
+
+			newKey := TokenKey{
+				ID:         uuid.NewString(),
+				PrivateKey: privateKey,
+				PublicKey:  publicKey,
+				IsPicked:   false,
+			}
+
+			if err := db.Create(&newKey).Error; err != nil {
+				log.Println("Error inserting new key:", err)
+			} else {
+				fmt.Println("Added new key:", publicKey)
+			}
+			continue
+		}
+
+		time.Sleep(10 * time.Minute)
+	}
+}
+
+func main() {
+	db := connectDB()
+	go checkAndGenerateKeys(db)
+	select {}
+}
